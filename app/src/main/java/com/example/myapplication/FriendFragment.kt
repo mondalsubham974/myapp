1ad1ef3e8bcbaf952ba63fb50fd4da67b8a4694b
mondalsubham974 <63149144+mondalsubham974@users.noreply.github.com>
2020-08-14 18:39:12 +0530

    Update FriendFragment.kt

--- app/src/main/java/com/example/myapplication/FriendFragment.kt
+++ app/src/main/java/com/example/myapplication/FriendFragment.kt
@@ -1,59 +1,50 @@
 package com.example.myapplication
 
-import android.os.Bundle
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FriendRequestFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FriendFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+class FriendAdapter(private val mcontext: Context, private val friendList:List<String>,
+                    private val isChatCheck:Boolean):RecyclerView.Adapter<FriendAdapter.ViewHolder?>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(mcontext).inflate(R.layout.activity_friend_item_layout, viewGroup, false)
+        return ViewHolder(view)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend_request, container, false)
+    override fun getItemCount(): Int {
+        return friendList.size
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FriendRequestFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FriendRequestFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onBindViewHolder(holder: ViewHolder, i: Int) {
+        val user = friendList[i]
+        val dbRef = FirebaseDatabase.getInstance().reference.child("User/$user")
+        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {}
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val confirmFriendUser = snapshot.getValue(Users::class.java)
+                holder.usernameTxt.text = confirmFriendUser!!.username
+                Picasso.get().load(confirmFriendUser.profile).placeholder(R.drawable.blank_profile_picture).into(holder.profileImageView)
             }
+        })
+    }
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        var usernameTxt: TextView = itemView.findViewById(R.id.activity_user_item_layout_username)
+        var profileImageView: CircleImageView = itemView.findViewById(R.id.activity_user_item_layout_ProfileImage)
+        var onlineImageView: CircleImageView = itemView.findViewById(R.id.activity_user_item_layout_online)
+        var offlineImageView: CircleImageView = itemView.findViewById(R.id.activity_user_item_layout_offline)
+        var lastMessageTxt: TextView = itemView.findViewById(R.id.activity_user_item_layout_lastMessage)
+        var addFriendButton: Button = itemView.findViewById(R.id.add_friend)
+
     }
 }
